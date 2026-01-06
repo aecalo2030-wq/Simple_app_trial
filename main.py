6525952a32aecff50ae0a7ac1ebf23cdd55dc3e3
aecalo2030-wq <aecalo2030@crc.pshs.edu.ph>
2026-01-06 09:00:56 +0800

    Add distance calculation, password checker, and loan eligibility
    Refactored the program to include distance calculation, password checker, and loan eligibility check.

--- main.py
+++ main.py
@@ -1,8 +1,49 @@
-number = int(input("Input a number"))
+Math, distance between 2 points:
+import math
 
-if number % 2 == 0:
-    print("even")
-elif number % 2 != 0:
-    print("Odd")
+# Step 1: Get user input
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+
+# Step 2: Apply the Euclidean distance formula
+distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
+
+# Step 3: Display the result with 2 decimal places
+print(f"The distance between the two points is: {distance:.2f}")
+# End the program
+
+
+Password Checker:
+
+def password_checker():
+    while True:
+        password = input("Enter your password: ")
+        
+        if len(password) < 8:
+            print("passwordsword is too short, must be atleast 8 characters long. Try again")
+        elif len(password) > 15:
+            print("password is too long. Must not exceed 15 characters. Try again")
+        else:
+            print("Password accepted.")
+            break
+password_checker()
+
+Loan Eligiility:
+credit_score = int(input("Enter credit score: "))
+income = float(input("Enter annual income: "))
+years_job = int(input("Enter years at current job: "))
+
+if credit_score >= 700:
+    if income >= 30000:
+        if years_job >= 2:
+            print("Result: Loan Approved")
+        else:
+            print("Result: Loan Denied")
+    else:
+        print("Result: Loan Denied")
 else:
-    print("Enter a number please")
+    print("Result: Loan Denied")
+
+
